f139b03630130fa602b3e2339cad7c1f274da101
Juan <jmartinez6784@yahoo.com>
2020-07-02 22:25:33 -0700

    Create Flights and Airport through seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+airport_collection = [
+  Airport.create!(code: "LAX"), 
+  Airport.create!(code: "SFO"), 
+  Airport.create!(code: "LAS"), 
+  Airport.create!(code: "SLC"), 
+  Airport.create!(code: "DEN"), 
+  Airport.create!(code: "ICT"), 
+  Airport.create!(code: "STL"), 
+  Airport.create!(code: "ORD"), 
+  Airport.create!(code: "TPA"), 
+  Airport.create!(code: "JFK")
+]
+
+valid_pairs = []
+# Pair up valid pairs of Airports 5000 times
+5000.times do
+  from = airport_collection[rand(0..9)]
+  to = airport_collection[rand(0..9)]
+
+  next if from == to
+
+  valid_pairs << [from, to]
+end
+
+valid_pairs.each do |valid_pair|
+  from = valid_pair[0]
+  to = valid_pair[1]
+
+  # Random Day, hour and minute of flight
+  r_days = rand(7..180)
+  r_hours = rand(1..23)
+  r_minutes = rand(1..59)
+  # Random flight duration
+  r_duration = rand(2..10)
+
+  Flight.create!(
+    departure_id: from.id, arrival_id: to.id, 
+    start_date: Date.today + r_days.days, 
+    start_time: (Date.today + r_days.days + r_hours.hours + r_minutes.minutes).strftime("%H:%M:%S"), 
+    end_date: (Date.today + r_days.days + r_hours.hours + r_minutes.minutes + r_duration.hours).strftime("%Y-%m-%d"), 
+    end_time: (Date.today + r_days.days + r_hours.hours + r_minutes.minutes + r_duration.hours).strftime("%H:%M:%S")
+  )
+end
