7147eef5e3b8811d8ff87d628f97191f5537a302
Juan <jmartinez6784@yahoo.com>
2020-07-03 23:56:43 -0700

    Added associations for bookings, passengers, and flight

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
-  belongs_to :passenger
   belongs_to :flight
+  has_and_belongs_to_many :passengers
+  accepts_nested_attributes_for :passengers
+
+  validates :passenger_id, :flight_id, presence: true
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :from, foreign_key: "departure_id", class_name: "Airport"
   belongs_to :to, foreign_key: "arrival_id", class_name: "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   validates :departure_id, :arrival_id, :start_date, :start_time, :end_date,:end_time, presence: true
 end
