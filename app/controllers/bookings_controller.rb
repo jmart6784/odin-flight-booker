932b9ea98700db48dc3ae99a2751d55b30c33777
Juan <jmartinez6784@yahoo.com>
2020-07-13 12:46:28 -0700

    Styled Bookings index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,11 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @bookings = Booking.all.paginate(page: params[:page], per_page: 12)
   end
 
   def show
     @booking = Booking.find(params[:id])
     @flight = Flight.find(@booking.flight_id)
-    @passenger = Passenger.find(@booking.passenger_id)
   end
 
   def new
@@ -164,29 +163,3 @@ class BookingsController < ApplicationController
     )
   end
 end
-
-
-
-
-# def create
-#   @passenger = Passenger.new(name: params[:passengers][:name], email: params[:passengers][:email])
-
-#   if @passenger.save
-#     @booking = Booking.new(flight_id: params[:flight_id].to_i)
-#     @booking.flight_id = params[:flight_id].to_i
-#     @booking.passenger_id = @passenger.id
-
-#     if @booking.save
-#       @passenger.booking_id = @booking.id
-#       @passenger.save
-#       flash.notice = "Successfully created a booking!"
-#       redirect_to @booking
-#     else
-#       flash.now[:danger] = "Can't create this Booking"
-#       render "new"
-#     end
-#   else
-#     flash.now[:danger] = "Can't create this Booking"
-#     render "new"
-#   end
-# end
