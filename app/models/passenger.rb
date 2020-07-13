4d03151d4632f3ec5998370596be15b91523dbb2
Juan <jmartinez6784@yahoo.com>
2020-07-12 17:35:41 -0700

    Styled nav bar and added background image

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,5 +3,4 @@ class Passenger < ApplicationRecord
   has_many :flights, through: :bookings
 
   validates :name, :email, presence: true
-  # validates :name, :email, :booking_id, presence: true
 end
